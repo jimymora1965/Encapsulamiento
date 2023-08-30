9b69989f9402f3a7ddae3527665918bf430b71d5
jimy mora <jimymora@hotmail.com>
2023-08-30 15:04:31 -0500

    encapsulamiento auto

---
+++ __pycache__/preview/getter_setter/get_set_auto.py
@@ -0,0 +1,30 @@
+class Auto:
+    def __init__(self, modelo, año):
+        self._m = modelo
+        self._a = año
+
+    def get_modelo(self):
+        return self._m
+    
+    def get_año(self):
+        return self._a 
+    
+    def set_modelo(self,new_modelo):
+        self._m = new_modelo
+
+    def set_año(self, new_año):
+        self._a = new_año
+
+#creamos el objeto  y  lo llamamos con sus caracteristicas
+vehiculo = Auto("Aveo", 2009)
+modelo = vehiculo.get_modelo()
+año = vehiculo.get_año()
+print("El modelo es: " , modelo, "Del año: ", año)
+
+#modificamoslos valores con set
+vehiculo.set_modelo("Cruz")
+vehiculo.set_año(2023)
+modelo = vehiculo.get_modelo()
+año = vehiculo.get_año()
+print(modelo,año)
+
